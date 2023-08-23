f9a4daef7057d80c669ab15b94c93eb422bfe327
Arafat-Rahman <arafat.cuet99@gmail.com>
2023-08-23 21:09:55 +0600

    Additional Functionality Done

--- index.js
+++ index.js
@@ -8,23 +8,26 @@ const searchFood = () =>{
     // console.log(url);
     fetch(url)
     .then(res => res.json())
-    .then(data => displayFood(data.meals))
+    .then(data => displayFood(data.meals, inputValue))
 }
 
-const displayFood = (foods) => {
+const displayFood = (foods, inputValue) => {
     // console.log(foods);
     const errorArea = document.getElementById('error-area');
+    errorArea.textContent = "";
     const span = document.createElement('span');
     span.classList.add('danger');
+    const resultField = document.getElementById('search-result');
+    resultField.textContent = "";
+    
     if(foods === null){
         span.innerHTML = `
-            The Food is not Found
+            The Food ${inputValue} is not Found
         `
 
         errorArea.appendChild(span);
     }else{
-        const resultField = document.getElementById('search-result');
-        resultField.textContent = "";
+
         foods.forEach(food => {
             // console.log(food);
             const div = document.createElement('div');
@@ -35,7 +38,7 @@ const displayFood = (foods) => {
                     <div class="card-body">
                         <h5 class="card-title">${food.strMeal}</h5>
                         <p class="card-text">${food.strInstructions.slice(0, 150) + "<strong> <i>see more</i></strong>"}</p>
-                        <button onclick="foodDetails(${food.idMeal})" class="btn btn-outline-dark w-100">See More Details</button>
+                        <a href="#food-details"> <button onclick="foodDetails(${food.idMeal})" class="btn btn-outline-dark w-100" >See More Details</button></a>
                     </div>
                 </div>
             `
@@ -60,6 +63,7 @@ const displaySingleFood = (food) => {
     foodDetails.textContent = "";
     const singleFoodContainer = document.createElement('div');
     singleFoodContainer.classList.add('card');
+    // singleFoodContainer.setAttribute('id', 'details');
     singleFoodContainer.innerHTML = `
 
             <img src=${food.strMealThumb} class="card-img-top" alt=${food.strMeal}>
@@ -70,3 +74,11 @@ const displaySingleFood = (food) => {
     `
     foodDetails.appendChild(singleFoodContainer);
 }
+
+var input = document.getElementById("searchInput");
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("myBtn").click();
+  }
+});
